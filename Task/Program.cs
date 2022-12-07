52a16abb9bcf2dc7c14d7506e86b1b822480899c
marydzhen <marydzhen@bk.ru>
2022-12-08 01:14:23 +0700

    program

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿// ["Hello","2", "world", ":-)"] → ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] → ["-2"]
+// ["Russia", "Denmark", "Kazan"] → []
+string[] array1 = new string[] {"Hello","2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+int MAX_WORD_LENGTH = 3;
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= MAX_WORD_LENGTH)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
